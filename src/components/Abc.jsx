f994ed2b063ff72e13d0118a04792e6989d37df9
Kabeer Ali Alvi <kabeeralvi6568@gmail.com>
2022-12-10 23:41:45 +0500

    Modal added

--- src/components/Abc.jsx
+++ src/components/Abc.jsx
@@ -9,30 +9,13 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { DownOutlined, SmileOutlined } from '@ant-design/icons';
 import { Dropdown, Space, Button, Modal } from 'antd';
 
-const items = [
-    {
-        key: '1',
-        label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-                Login
-            </a>
-        ),
-    },
-    {
-        key: '2',
-        label: (
-            <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-                Signup
-            </a>
-        ),
-    }
-
-];
 
 
 
 
 const MainHeader = () => {
+
+    
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const showModal = () => {
@@ -45,6 +28,54 @@ const MainHeader = () => {
         setIsModalOpen(false);
     };
 
+
+    const [isModalOpen2, setIsModalOpen2] = useState(false);
+    const showModal2 = () => {
+        setIsModalOpen2(true);
+    };
+    const handleOk2 = () => {
+        setIsModalOpen2(false);
+    };
+    const handleCancel2 = () => {
+        setIsModalOpen2(false);
+    };
+
+
+    const items = [
+        {
+            key: '1',
+            label: (
+                <>
+                <a type="primary" onClick={showModal2}>
+        Open Modal
+      </a>
+      <Modal title="Basic Modal" open={showModal2} onOk={handleOk2} onCancel={handleCancel2}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+      </>
+            ),
+        },
+        {
+            key: '2',
+            label: (
+                <>
+                <a type="primary" onClick={showModal}>
+        Open Modal
+      </a>
+      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents.2</p>
+        <p>Some contents2</p>
+        <p>Some contents.2</p>
+      </Modal>
+      </>
+            ),
+        }
+    
+    ];
+
+    
     return (
         <>
             <Navbar style={{ backgroundColor: '#f4f4f4' }}>
